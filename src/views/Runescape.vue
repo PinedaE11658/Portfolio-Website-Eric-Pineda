5cdf739eb07af63c65040899a28cc77b21c85f19
PinedaE11658 <153141794+PinedaE11658@users.noreply.github.com>
2025-05-26 17:10:50 -0500

    Create Runescape.vue

---
+++ src/views/Runescape.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="runescape-box">
+    <h1><u>Runescape Accounts</u></h1>
+    <p>Hello! This part of the website is currently under construction</p>
+   
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.runescape-box {
+  background-color: #FCF4DD;
+  padding: 100px;
+  border-radius: 20px;
+  max-width: 500px;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+</style>
